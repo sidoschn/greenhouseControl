63efc2ba03035e586da472e04ce2ae0e35facadd
sidoschn <dominik.schneidereit@fau.de>
2026-05-20 15:29:50 +0200

    added sensor ID tool

---
+++ identifySensors.py
@@ -0,0 +1,19 @@
+from ds18b20 import DS18B20
+
+
+sensorList = DS18B20.get_available_sensors()
+
+
+
+
+print(str(len(sensorList))+" Sensors found")
+
+for sensor in sensorList:
+    id = sensor.get_id()
+    temp = sensor.get_temperature()
+    print("Sensor with ID: " +id)
+
+    print("reading temperature: "+ temp)
+
+
+
