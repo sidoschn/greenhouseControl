ac824d2ce8b425c1726e54f743a4f0a3a64a2d22
sidoschn <dominik.schneidereit@fau.de>
2026-05-20 15:43:15 +0200

    fixed the tool to not initialize sensors and but only display ids

--- identifySensors.py
+++ identifySensors.py
@@ -9,11 +9,11 @@ sensorList = DS18B20.get_available_sensors()
 print(str(len(sensorList))+" Sensors found")
 
 for sensor in sensorList:
-    id = sensor.get_id()
-    temp = sensor.get_temperature()
-    print("Sensor with ID: " +id)
+    #id = sensor.get_id()
+    #temp = sensor.get_temperature()
+    print("Sensor with ID: " +sensor)
 
-    print("reading temperature: "+ temp)
+    #print("reading temperature: "+ temp)
 
 
 
